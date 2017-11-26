3e0c84b242a9993a380a480d645d7f37c7f4cce1
umit aykurt <umitaykurt1993@gmail.com>
2017-11-26 22:08:36 +0300

    Simple functionality added, sends file

---
+++ FileServer/client.c
@@ -0,0 +1,88 @@
+/*
+    C ECHO client example using sockets
+*/
+#include <stdio.h> //printf
+#include <string.h>    //strlen
+#include <sys/socket.h>    //socket
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int createAndOpenFile( const char* filename ){
+
+	int fd;
+	if( ( fd = open( filename, O_WRONLY | O_CREAT | O_TRUNC ) ) != -1 ){
+
+			return fd;
+	}
+
+	return -1;
+
+}
+ 
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char file_name[50] , file_content[2000];
+	
+	char new_file[20] = "umit_read.txt";
+     
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
+ 
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+     
+    puts("Connected\n");
+     
+    //keep communicating with server
+    while(1)
+    {
+        printf("Enter filename : ");
+        //scanf("%s\n" , message);
+        fgets( file_name, 50, stdin );
+
+		int fd = createAndOpenFile( "send_from_server" );
+		
+        //Send some data
+        if( send(sock , file_name , strlen(file_name) , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+         
+        //Receive a reply from the server
+        if( recv(sock , file_content , 2000 , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }
+
+		printf( "Received file is %s\n", file_content );
+		
+		
+		write( fd, file_content, strlen( file_content ) );
+
+		close( fd );
+
+         
+    }
+     
+    close(sock);
+    return 0;
+}
--- FileServer/file_server.c
+++ FileServer/file_server.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include<pthread.h>
 #include<fcntl.h>
+#include<errno.h>
 
 
 #define FILENAME_SIZE 50
@@ -22,40 +23,58 @@ const char* error_message_read_file = "Could not read the file";
 
 void* workerThread( void* arg ){
 
-	int client_socket = ( int* )arg;
-
+	int read_bytes;
+	int client_socket = *( int* )arg;
 	char filename[FILENAME_SIZE];
+	char file[300];
 
-    int result = read(client_socket , filename ,FILENAME_SIZE );
+	while( 1 ){
+   		 int result = read(client_socket , filename ,FILENAME_SIZE );
 
-	if( result == -1 ){
-		send( client_socket, error_message_read_name, strlen( error_message_read_name ), 0 );
+		if( result == 0 || result == -1 ){
 
-		pthread_exit( ( void* )1 );
-	}
+			printf("-------------------Closing connection---------------------\n");
+			close( client_socket );
+			pthread_exit( 0 ); 
+		}
 
-	char file[BUFFER_SIZE];
-	int fd;
+	     filename[result-1] = '\0';
 
-	if( ( fd = open( filename, O_RDONLY ) ) != -1 ){
+	     printf( "Read from socket data %s and its size is %d\n", filename, strlen(filename)  );
 
-		if( read( fd, file, BUFFER_SIZE ) != -1 ){
 
+		if( result == -1 ){
+			send( client_socket, error_message_read_name, strlen( error_message_read_name ), 0 );
+
+			pthread_exit( ( void* )1 );
+		}
+
+
+		printf( "Requested file is %s\n", filename );
+
+	
+		int fd = open( filename, O_RDONLY );
+
+		if( fd == -1 ) {
+			perror( "ERROR: " );
+			send( client_socket, error_message_open_file, strlen( error_message_open_file), 0 );
+			continue;
+		}
+
+		int bytes;
+		if( ( bytes = read( fd, file, 300 ) ) != -1 ) {
+
+			file[bytes-1] = '\0';
 			send( client_socket, file, strlen( file ), 0 );		
 		}
 
 		else{
-
-			pthread_exit( ( void* )1 );
-		}		
-
-	} 
-
-	else{
-
-		send( client_socket, error_message_open_file, strlen( error_message_open_file ), 0 ); 
-		pthread_exit( ( void* ) 1 );
-	} 
+			
+			send( client_socket, file, strlen( file ), 0 );
+			
+		}
+		
+	}
 
 	pthread_exit( ( void*) 0 );
 
@@ -103,9 +122,12 @@ int main(int argc , char *argv[])
     //accept connection from an incoming client
     client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
 
-	int id;	
+	puts("Connection accepted");
+
+	pthread_t id;	
 
 	pthread_create( &id, NULL, workerThread, &client_sock );
+	pthread_join( id, NULL );
 
     return 0;
 }
