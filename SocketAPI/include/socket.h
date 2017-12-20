140e7cdffff9d7a552524e3219f23b21e607cda7
umtaykrt1993 <umitaykurt1993@gmail.com>
2017-12-20 10:38:48 +0300

    Changed methods to get as a parameter socket addresses

--- SocketAPI/include/socket.h
+++ SocketAPI/include/socket.h
@@ -1,14 +1,15 @@
 
 #ifndef _SOCKET_H
 #define _SOCKET_H
+#include<arpa/inet.h>
 
 typedef int socket_desc;
 
 socket_desc openTCPSocket();
 
-int connectToServer( socket_desc socket_descriptor, const char* server_address, int port_number );
+int connectToServer( socket_desc socket_descriptor,struct sockaddr_in* addr, const char* server_address, int port_number );
 
-int bindSocket( socket_desc socket, int port_number );
+int bindSocket( socket_desc socket, struct sockaddr_in* addr, int port_number );
 
 
 #endif
--- SocketAPI/src/socket.c
+++ SocketAPI/src/socket.c
@@ -1,6 +1,5 @@
 #include "socket.h"
 #include <sys/socket.h>
-#include <arpa/inet.h>
 
 
 socket_desc openTCPSocket(){
@@ -12,27 +11,25 @@ socket_desc openTCPSocket(){
 
 }
 
-int connectToServer( socket_desc socket_descriptor, const char* server_address, int port_number ){
+int connectToServer( socket_desc socket_descriptor, struct sockaddr_in* addr ,const char* server_address, int port_number ){
 
-	struct sockaddr_in server_add;
-	server_add.sin_addr.s_addr = inet_addr( server_address );
-	server_add.sin_family = AF_INET;
-	server_add.sin_port = htons( port_number );
+	addr->sin_addr.s_addr = inet_addr( server_address );
+	addr->sin_family = AF_INET;
+	addr->sin_port = htons( port_number );
 
-	int return_val = connect( socket_descriptor, ( struct sockaddr*)&server_add, sizeof( struct sockaddr_in ) );
+	int return_val = connect( socket_descriptor, ( struct sockaddr*)addr, sizeof( struct sockaddr_in ) );
 
 	return return_val;
 
 }
 
-int bindSocket( socket_desc socket, int port_number ){
+int bindSocket( socket_desc socket, struct sockaddr_in* addr, int port_number ){
 
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons( port_number );
+	addr->sin_family = AF_INET;
+	addr->sin_addr.s_addr = INADDR_ANY;
+	addr->sin_port = htons( port_number );
 
-	int return_code = bind( socket, ( struct sockaddr* )&addr, sizeof( addr ) );
+	int return_code = bind( socket, ( struct sockaddr* )addr, sizeof( struct sockaddr_in ) );
 
 	return return_code;
 
