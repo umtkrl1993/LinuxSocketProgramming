b41a63fb6685f9f51575fd591e1018eba0637282
umtaykrt1993 <umitaykurt1993@gmail.com>
2018-01-23 09:23:01 +0300

    separated defs into a defs.h file

---
+++ FileServer/defs.h
@@ -0,0 +1,16 @@
+#ifndef _DEFS_H
+#define _DEFS_H
+
+#define FILENAME_SIZE 50
+#define BUFFER_SIZE 2000
+#define SERVER_PORT 1100
+#define DATABASE_PORT 1194
+#define DATABASE_HOST "127.0.0.1"
+
+const char* error_message_read_name = "Could not read file name";
+const char* error_message_open_file = "Could not open the file";
+const char* error_message_read_file = "Could not read the file";
+const char* user_name_question = "Please enter username :";
+const char* password_question = " Please enter password :";
+
+#endif
